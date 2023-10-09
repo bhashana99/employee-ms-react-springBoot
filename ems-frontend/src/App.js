c689a4c8649c6cebcb32714f15c6d4e7504ee15f
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-09 23:44:09 +0530

    import footer component to app.js

--- ems-frontend/src/App.js
+++ ems-frontend/src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import AllEmployeeComponents from './components/AllEmployeeComponents';
 import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+
 function App() {
   return (
     <div >
       <HeaderComponent />
       <AllEmployeeComponents />
+      <FooterComponent />
     </div>
   );
 }
