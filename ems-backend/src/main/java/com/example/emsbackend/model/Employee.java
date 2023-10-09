f7b7edad5b057bf39a6d904cb2cb601f6a43aca2
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-09 21:09:28 +0530

    add employee model details and column names

--- ems-backend/src/main/java/com/example/emsbackend/model/Employee.java
+++ ems-backend/src/main/java/com/example/emsbackend/model/Employee.java
@@ -1,2 +1,26 @@
-package com.example.emsbackend.model;public class Employee {
+package com.example.emsbackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
 }
