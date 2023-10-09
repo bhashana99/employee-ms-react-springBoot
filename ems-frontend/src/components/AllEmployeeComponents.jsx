fc6a64fdabbe5930e0c7a875c169390ca2b99daf
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-09 22:31:02 +0530

    import to allEmployeeComponent to app.js

---
+++ ems-frontend/src/components/AllEmployeeComponents.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllEmployeeComponents = () => {
+  return (
+    <div>
+      <h2>All Employee Component</h2>
+    </div>
+  )
+}
+
+export default AllEmployeeComponents
