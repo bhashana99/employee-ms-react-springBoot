12f0579ab622f54b8d5e9b4bd2e0ce87dd90b4fa
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-09 21:32:41 +0530

    change request mapping location

--- ems-backend/src/main/java/com/example/emsbackend/controller/EmployeeController.java
+++ ems-backend/src/main/java/com/example/emsbackend/controller/EmployeeController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/employees")
 public class EmployeeController {
 
     @Autowired
     private EmployeeRepo employeeRepo;
+
+
 
 }
