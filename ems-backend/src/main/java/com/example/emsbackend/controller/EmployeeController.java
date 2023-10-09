c5814121874fc42c26bdad044b4e5dfa3254018e
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-09 21:30:14 +0530

    add RequestMapping annotation to EmployeeController Class

--- ems-backend/src/main/java/com/example/emsbackend/controller/EmployeeController.java
+++ ems-backend/src/main/java/com/example/emsbackend/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.emsbackend.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/")
 public class EmployeeController {
 
 }
