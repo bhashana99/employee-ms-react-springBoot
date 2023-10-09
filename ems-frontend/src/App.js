3b196b331db97c8cdb6342477f6a95255eaba772
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-09 23:35:26 +0530

    Add HeaderComponent to App.js

--- ems-frontend/src/App.js
+++ ems-frontend/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import AllEmployeeComponents from './components/AllEmployeeComponents';
-
+import HeaderComponent from './components/HeaderComponent';
 function App() {
   return (
     <div >
+      <HeaderComponent />
       <AllEmployeeComponents />
     </div>
   );
