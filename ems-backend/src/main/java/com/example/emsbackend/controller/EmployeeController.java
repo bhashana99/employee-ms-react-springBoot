acbce366e8d08e7d81284dc9d42c9604f2b816b6
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-09 21:37:34 +0530

    Get All Employees Details API Request

--- ems-backend/src/main/java/com/example/emsbackend/controller/EmployeeController.java
+++ ems-backend/src/main/java/com/example/emsbackend/controller/EmployeeController.java
@@ -1,9 +1,13 @@
 package com.example.emsbackend.controller;
 
+import com.example.emsbackend.model.Employee;
 import com.example.emsbackend.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/employees")
@@ -11,6 +15,13 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeRepo employeeRepo;
+
+    //Get All Employees Details API Request
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+
+        return employeeRepo.findAll();
+    }
 
 
 
