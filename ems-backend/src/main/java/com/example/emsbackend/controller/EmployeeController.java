e4956ed7a58640f467a837ae3bc45dedb2edfb7d
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-09 21:31:30 +0530

    inject Employee repo

--- ems-backend/src/main/java/com/example/emsbackend/controller/EmployeeController.java
+++ ems-backend/src/main/java/com/example/emsbackend/controller/EmployeeController.java
@@ -1,10 +1,15 @@
 package com.example.emsbackend.controller;
 
+import com.example.emsbackend.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepo employeeRepo;
 
 }
