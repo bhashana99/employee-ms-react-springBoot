8bb7fc8d2d10fe4514111644bd8e3c52a9b0e4dd
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-10 08:54:11 +0530

    import Routes and Route

--- ems-frontend/src/App.js
+++ ems-frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router , Route ,Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import AllEmployeeComponents from './components/AllEmployeeComponents';
 import HeaderComponent from './components/HeaderComponent';
@@ -8,8 +8,15 @@ import FooterComponent from './components/FooterComponent';
 function App() {
   return (
     <div >
-      <Router></Router>
-      <HeaderComponent />
+      <Router>
+        <HeaderComponent />
+        <div className='container'>
+          <Routes>
+
+          </Routes>
+        </div>
+      </Router>
+      
       <AllEmployeeComponents />
       <FooterComponent />
     </div>
