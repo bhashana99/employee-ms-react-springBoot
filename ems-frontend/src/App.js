f60da705b38f66196b4a12cdf2808b08f139da59
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-10 08:51:48 +0530

    import Router

--- ems-frontend/src/App.js
+++ ems-frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import AllEmployeeComponents from './components/AllEmployeeComponents';
 import HeaderComponent from './components/HeaderComponent';
