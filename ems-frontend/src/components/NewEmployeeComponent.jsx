418c58edeb8ebcd2f3edf4a68055671bf408ff6d
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-10 12:00:13 +0530

    import useEffect

--- ems-frontend/src/components/NewEmployeeComponent.jsx
+++ ems-frontend/src/components/NewEmployeeComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import EmployeeService from '../service/EmployeeService';
 import Swal from 'sweetalert2';
