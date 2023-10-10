4a42aebe13519db634d80307491390c60565362b
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-10 10:04:18 +0530

    addEmployee defined to save data

--- ems-frontend/src/service/EmployeeService.js
+++ ems-frontend/src/service/EmployeeService.js
@@ -6,6 +6,11 @@ class Employees {
   getEmployees() {
     return axios.get(EMPLOYEE_API_BASE_URL);
   }
+
+  addEmployee(employee) {
+    return axios.post(EMPLOYEE_API_BASE_URL, employee);
+  }
+
 }
 
 export default new Employees();
