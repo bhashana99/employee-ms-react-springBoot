43e6eb12672cf4fa1db4f93bed5c055ac841ee20
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-10 08:50:17 +0530

    Start WOrking on Router

--- ems-frontend/src/App.js
+++ ems-frontend/src/App.js
@@ -7,6 +7,7 @@ import FooterComponent from './components/FooterComponent';
 function App() {
   return (
     <div >
+      <Router></Router>
       <HeaderComponent />
       <AllEmployeeComponents />
       <FooterComponent />
